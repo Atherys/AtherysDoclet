6df80f46691d271c2547271db3b9fbd69f0ecafa
Liam <liamreadsspam@gmail.com>
2018-09-09 15:24:29 -0300

    Add Module class to represent a module (category)

---
+++ src/main/java/Module.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Module {
+    private String name;
+    private File file;
+    private PrintWriter writer;
+
+    public Module(String name) {
+        this.name = name;
+        this.file = new File("docs/" + name);
+        Log.info("hello");
+        try {
+            this.writer = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            Log.error("File not found for module " + name);
+        }
+    }
+
+    public void writeln(String line) {
+        writer.println(line);
+    }
+
+    public void close() {
+        writer.close();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
