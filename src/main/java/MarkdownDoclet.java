fdc5459c1df232d137a6a31752169ce946285b8e
Liam <liamreadsspam@gmail.com>
2018-09-10 18:30:22 -0300

    Test if method overrides, clean up

--- src/main/java/MarkdownDoclet.java
+++ src/main/java/MarkdownDoclet.java
@@ -11,13 +11,14 @@ import java.util.Map;
 public class MarkdownDoclet {
 
     private static Map<String, Module> modules = new HashMap<>();
+    private static final String functionTag = "jsfunction";
 
     public static boolean start(RootDoc start) {
         int functionFiles = 0;
-        FileUtils.makeDir("docs");
+        Utils.makeDir("docs");
 
         for (ClassDoc classDoc : start.classes()) {
-            if (classDoc.tags("func").length > 0) {
+            if (classDoc.tags("jsfunction").length > 0) {
                 handleClass(classDoc);
                 functionFiles++;
             }
@@ -35,7 +36,7 @@ public class MarkdownDoclet {
 
     private static void handleClass(ClassDoc classDoc) {
         for (MethodDoc methodDoc : classDoc.methods()) {
-            if (methodDoc.tags("func").length > 0) {
+            if (methodDoc.overriddenMethod() != null) {
                 String module = classDoc.containingPackage().name();
                 module = module.substring(module.lastIndexOf(".") + 1);
                 if (!modules.containsKey(module)) {
