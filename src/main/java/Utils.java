06269edb444165b0d644519bf831a057618209bf
Liam <liamreadsspam@gmail.com>
2018-09-10 18:29:41 -0300

    Add split util

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,8 +1,13 @@
 import java.io.File;
 
-public class FileUtils {
+public class Utils {
     public static void makeDir(String name) {
         File dir = new File(name);
         dir.mkdir();
+    }
+
+    public static String split(String tagText) {
+        int index = tagText.indexOf(" ");
+        return tagText.substring(0, index) + ":" + tagText.substring(index);
     }
 }
